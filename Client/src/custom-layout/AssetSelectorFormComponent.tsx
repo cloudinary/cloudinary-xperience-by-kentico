9452c1ad1cbadd2e7e4baac047048cf83ed6325a
Creston Davison <cdavison@laughlin.com>
2026-06-08 12:06:36 -0500

    fix:empty json string errors in page builder props

--- Client/src/custom-layout/AssetSelectorFormComponent.tsx
+++ Client/src/custom-layout/AssetSelectorFormComponent.tsx
@@ -58,7 +58,7 @@ export const AssetSelectorFormComponent = (
               username: props.userName,
               use_saml: props.useSaml,
               saml_iframe_support: props.useIframeSupportForSaml,
-              default_transformations: JSON.parse(props.defaultTransformations),
+              default_transformations: props.defaultTransformations.trim() !== "" ? JSON.parse(props.defaultTransformations) : "",
             }}
             onSelect={handleOnChange}
             disabled={props.errorMessage != null}
